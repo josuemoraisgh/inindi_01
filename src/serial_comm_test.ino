def9f1e21e702d7b22e74c426c8817bc701b361b
josuemoraisgh <josuemorais@gmail.com>
2024-03-27 13:22:15 -0300

    refactor inindThread.h

--- src/serial_comm_test.ino
+++ src/serial_comm_test.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <Arduino.h>
 
 void setup()
@@ -27,33 +26,3 @@ void loop()
 
   delay(50);
 }
-=======
-#include <Arduino.h>
-
-void setup()
-{
-  Serial.begin(115200);
-  // Print log
-  Serial.println("setup");
-}
-
-float i = 0;
-void loop()
-{
-  i += 0.1;
-
-  // Print log
-  Serial.print("casa");
-  Serial.println(i);
-
-  // Plot a sinus
-  Serial.print(">sin:");
-  Serial.println(sin(i));
-
-  // Plot a cosinus
-  Serial.print(">Sum:");
-  Serial.println(sin(i)+cos(i));
-
-  delay(50);
-}
->>>>>>> 0d43539a8abd1a10aaa09c064baf6a1a280091bd
